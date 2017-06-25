d1f4ab1f6424a33dcb6f9ff70a522b4b366e400a
John Berger <johnmartinberger@gmail.com>
2017-06-25 13:20:48 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,12 +2,14 @@ import os
 import time
 import markovify
 import tweepy
+import random
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 # schedule tweets for every 2 hours
 sched = BlockingScheduler()
 @sched.scheduled_job('interval', minutes=120)
 def timed_job():
+
     # authenticate to twitter
     consumer_key = 'xxxx'
     consumer_secret = 'xxxx'
@@ -17,28 +19,32 @@ def timed_job():
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
 
-    # grab text to feed to markovify
+    # markovify
     with open("./whitman.txt") as f:
         text = f.read()
-   
-    # generate text model and markovify it
     text_model = markovify.Text(text)
-    
-    # use text model to make lines for poem
-    tweet = ''
-    for i in range(0, 4):
-        line = text_model.make_short_sentence(35)
-        while line == None:
-            line = text_model.make_short_sentence(35)
-        if i < 3:
-            line = line[:-1]
-        if i == 0:
-            tweet += line
-        else:
-            tweet += '\n' + line
 
-    # tweet
-    print(tweet)
-    api.update_status(tweet)
+    # randomly generate a 4-line poem or a 140-character string
+    boolean = random.choice([True, False])
+    
+    if boolean == True:
+        tweet = ''
+        for x in range(0, 4):
+            line = text_model.make_short_sentence(30)
+            while line == None:
+                line = text_model.make_short_sentence(30)
+            if x < 3:
+                line = line[:-1]
+            if x == 0:
+                tweet += line
+            else:
+                tweet += '\n' + line
+
+        print(tweet)
+        api.update_status(tweet)
+    else:
+        tweet = text_model.make_short_sentence(140)
+        print(tweet)
+        api.update_status(tweet)
 
 sched.start()
